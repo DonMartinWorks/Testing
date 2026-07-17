3e49c09c87cbb960ef27b013387dda42f82825e8
Epsilon <donmartinworks@gmail.com>
2026-07-17 16:04:45 -0400

    [TEST] - Test de login + Función de screenshots

--- tests/Feature/Routes/PublicPagesTest.php
+++ tests/Feature/Routes/PublicPagesTest.php
@@ -1,16 +1,41 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
 use function Pest\Laravel\get;
 
 it('shows welcome page', function () {
-    $response = get('/');
+    $response = visit('/');
 
-    $response->assertStatus(200);
+    $response->assertSee("Laravel");
 });
 
 it('shows the login page', function () {
-    get('/login')
-        ->assertStatus(200)
-        ->assertSee('Login')
+    visit('/login')
+        ->assertSee('Log in')
         ->assertDontSee('Dashboard');
+
+    visit('/')
+        ->click('Log in')
+        ->assertSee('Log in')
+        ->assertPathIs('/login');
+});
+
+it('tests that login works', function () {
+    $password = 'password123';
+
+    $user = User::factory()->create([
+        'password' => Hash::make($password),
+        'two_factor_secret' => null,
+        'two_factor_recovery_codes' => null,
+        'two_factor_confirmed_at' => null,
+    ]);
+
+    visit('/login')
+        ->type('email', $user->email)
+        ->type('password', $password)
+        ->press('Log in')
+        ->assertPathIs('/dashboard')
+        ->screenshot();
 });
