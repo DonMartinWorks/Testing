2abbc059a139458ce88e5cdd8a8ba5cd3aef497f
Epsilon <donmartinworks@gmail.com>
2026-07-20 19:33:31 -0400

    [TEST] - Test mobile y errores de consola

--- tests/Feature/Routes/PublicPagesTest.php
+++ tests/Feature/Routes/PublicPagesTest.php
@@ -39,3 +39,36 @@ it('tests that login works', function () {
         ->assertPathIs('/dashboard')
         ->screenshot();
 });
+
+it('tests that mobile menu works', function () {
+    $password = 'password123';
+
+    $user = User::factory()->create([
+        'password' => Hash::make($password),
+        'two_factor_secret' => null,
+        'two_factor_recovery_codes' => null,
+        'two_factor_confirmed_at' => null,
+    ]);
+
+    visit('/login')
+        ->on()->mobile()
+        ->type('email', $user->email)
+        ->type('password', $password)
+        ->press('Log in')
+        ->assertPathIs('/dashboard')
+        ->press('[data-slot=sidebar-trigger]')
+        ->assertVisible('Laravel Starter Kit')
+        ->screenshot();
+});
+
+it('tests that there no console logs and errors', function () {
+    $pages = visit(['/', '/login', '/register']);
+    [$home, $login, $register] = $pages;
+
+    $home->assertTitle('Welcome - Laravel');
+    $login->assertTitle('Log in - Laravel');
+    $register->assertTitle('Register - Laravel');
+
+    $pages->assertNoConsoleLogs()
+        ->assertNoJavaScriptErrors();
+});
